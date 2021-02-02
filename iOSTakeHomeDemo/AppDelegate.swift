d318f933114d6942355cb5cce4c0f6268f347620
Subodh <irshad@subcodevs.com>
2021-02-02 09:38:16 +0530

    initial commit

---
+++ iOSTakeHomeDemo/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  iOSTakeHomeDemo
+//
+//  Created by Irshad Ahmed on 02/02/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
